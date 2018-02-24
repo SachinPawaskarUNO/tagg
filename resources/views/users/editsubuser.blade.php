fd5d9faaae7c4afb18f64799cd2d1298b117ff6d
PoojaYalala <pyalala@unomaha.edu>
2018-02-24 16:07:55 -0600

    Added unique IDs

--- resources/views/users/editsubuser.blade.php
+++ resources/views/users/editsubuser.blade.php
@@ -36,18 +36,18 @@
                         <div class="form-group">
                             <label id = 'last_name' for="last_name" class="col-md-4 control-label"> Last Name <span style="color: red; font-size: 20px; vertical-align:middle;">*</span></label>
                             <div class="col-lg-6">
-                                {!! Form::text('last_name', null,['class' => 'form-control', 'required']) !!}</div>
+                                {!! Form::text('last_name', null,['class' => 'form-control', 'id' => 'last_name', 'required']) !!}</div>
                         </div>
 
                         <div class="form-group">
                             <label id = 'email'for="email" class="col-md-4 control-label">E-Mail Address <span style="color: red; font-size: 20px; vertical-align:middle;">*</span></label>
-                            <div class="col-lg-6">{!! Form::text('email', null, ['class' => 'form-control', 'required']) !!}</div>
+                            <div class="col-lg-6">{!! Form::text('email', null, ['class' => 'form-control', 'id' => 'email','required']) !!}</div>
                         </div>
 
                         <div class="form-group">
                             <label id = 'organization' for="organization_id" class="col-md-4 control-label">Business Location <span style="color: red; font-size: 20px; vertical-align:middle;">*</span></label>
                             <div class="col-lg-6">
-                                {!! Form::select('organization_id', $organizationStatusArray, $currentOrg, ['class' => 'form-control', 'id' => 'loc-drop-down', 'required']) !!}
+                                {!! Form::select('organization_id', $organizationStatusArray, $currentOrg, ['class' => 'form-control', 'id' => 'organization', 'id' => 'loc-drop-down', 'required']) !!}
                             </div>
                         </div>
 
