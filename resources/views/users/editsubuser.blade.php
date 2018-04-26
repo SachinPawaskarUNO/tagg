e2c3b089d5da1530e560e4c7cb734d1dcff7c2aa
AK <21245817+akhampariya@users.noreply.github.com>
2018-04-25 19:32:26 -0500

    Update editsubuser.blade.php

--- resources/views/users/editsubuser.blade.php
+++ resources/views/users/editsubuser.blade.php
@@ -67,7 +67,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-5">
                                 {!! Form::submit('Update', ['class' => 'btn updatebtn']) !!}
-                                <input class="btn backbtn" type="button" value="Cancel" onClick="history.go(-1);">
+                                <input class="btn backbtn" type="button" value="Cancel" onClick=location.href='{{ url('/dashboard')}}'>
                                 <span style="color: red"> <h5>Fields Marked With (<span style="color: red; font-size: 20px; vertical-align:middle;">*</span>) Are Mandatory</h5></span>
                             </div>
                         </div>
