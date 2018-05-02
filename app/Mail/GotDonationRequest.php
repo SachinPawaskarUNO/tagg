c0be0742964615fed3055e5d204a99986ae21d96
akhampariya <akhampariya@unomaha.edu>
2018-05-02 11:27:49 -0500

    Fix  - email from main biz

--- app/Mail/GotDonationRequest.php
+++ app/Mail/GotDonationRequest.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\DonationRequest;
 use App\User;
+use App\Organization;
 use App\ParentChildOrganizations;
 
 class GotDonationRequest extends Mailable
@@ -25,12 +26,17 @@ class GotDonationRequest extends Mailable
         
         $this->donationRequest = $donationRequest;
         // 
+
         $p_org = ParentChildOrganizations::select('parent_org_id')->where('child_org_id', $donationRequest->organization_id)->pluck('parent_org_id')->first();
         
         if(is_null($p_org)){
             $p_org =  $donationRequest->organization_id;
         }
+
+        $this->donationRequest->organization_id = $p_org;
+
         $esender = User::where('organization_id', $p_org)->firstOrFail();
+
         $this->from($esender->email);
         }
 
